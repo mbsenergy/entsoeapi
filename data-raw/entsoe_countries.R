8b4dd8bbbea3e79df346af19d653c3ae30ec52ca
Alejandro Abraham <a.abraham@mbsconsulting.it>
2024-12-10 22:51:03 +0100

    new api

--- data-raw/entsoe_countries.R
+++ data-raw/entsoe_countries.R
@@ -1,10 +1,12 @@
 # read raw tsv, trim the character columns and remove empty columns
-auction_types <- fs::path("data-raw",
-                          "auction_types",
-                          ext = "tsv") |>
+entsoe_countries <- fs::path("data-raw",
+                              "entsoe_countries",
+                              ext = "csv") |>
   data.table::fread(encoding = "UTF-8") |>
   purrr::modify_if(is.character, trimws, which = "both") |>
   purrr::discard(~is.na(.x) |> all())
 
+setnames(entsoe_countries, c('COUNTRY', 'EIC_CODE'))
+
 # save the package data in the correct format
-usethis::use_data(auction_types, overwrite = TRUE)
+usethis::use_data(entsoe_countries, overwrite = TRUE)
